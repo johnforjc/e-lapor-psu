e893d2cab1f87136a0846061c28e3a7c2d5a141e
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2020-08-23 22:07:35 +0700

    tabel notifikasi database

--- e_lapor_psu/pelaporan/models.py
+++ e_lapor_psu/pelaporan/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class DataPerusahaan(models.Model):
-    
     id_data_perusahaan = models.AutoField(primary_key=True)
     nama_perusahaan = models.CharField(max_length=255)
     akta_pendirian_badan_usaha = models.FileField(upload_to='akta_pendirian_badan_usaha')
@@ -28,14 +27,14 @@ class DataProyek(models.Model):
     target_pembangunan = models.IntegerField()
 
 class DataPerizinan(models.Model):
-    id_data_proyek = models.OneToOneField(DataProyek, on_delete=models.CASCADE, unique=True)
+    id_data_proyek = models.OneToOneField(DataProyek, on_delete=models.CASCADE, primary_key=True)
     site_plan = models.FileField(upload_to='site_plan')
     ukl_upl = models.FileField(upload_to='ukl_upl')
     izin_mendirikan_bangunan = models.FileField(upload_to='izin_mendirikan_bangunan')
     izin_penggunaam_bangunan = models.FileField(upload_to='izin_penggunaan_bangunan')
 
 class JenisPsu(models.Model):
-    id_data_proyek = models.OneToOneField(DataProyek, on_delete=models.CASCADE, unique=True)
+    id_data_proyek = models.OneToOneField(DataProyek, on_delete=models.CASCADE, primary_key=True)
     jaringan_jalan = models.FloatField()
     jaringan_saluran_pembuangan_air_hujan = models.FloatField()
     sanitasi = models.FloatField()
@@ -70,3 +69,8 @@ class RumahSusun(models.Model):
     tipe_rumah_susun = models.IntegerField()
     lb_rumah_susun = models.FloatField()
     jumlah_unit_rumah_susun = models.IntegerField()
+
+class Notifikasi(models.Model):
+    id_notifikasi = models.AutoField(primary_key=True)
+    id_data_perusahaan = models.ForeignKey(DataPerusahaan, on_delete=models.CASCADE)
+    isi_notifikasi = models.TextField()
