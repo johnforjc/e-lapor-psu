cfed208049106583c14fde91f738296f05663b6c
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2020-08-25 23:37:28 +0700

    update url pelaporan

--- e_lapor_psu/pelaporan/urls.py
+++ e_lapor_psu/pelaporan/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('form_data_perusahaan/', views.form_data_perusahaan, name='form_data_perusahaan'),
     path('form_data_proyek/', views.form_data_proyek, name='form_data_proyek'),
     path('form_data_perizinan/', views.form_data_perizinan, name='form_data_perizinan'),
-    path('generate_username/', views.generate_username, name="ganerate_username"),
     path('tipe_rumah_tapak/<int:id>/', views.tipe_rumah_tapak, name='tipe_rumah_tapak'),
     path('tipe_rumah_susun/<int:id>/', views.tipe_rumah_susun, name='tipe_rumah_susun'),
 
