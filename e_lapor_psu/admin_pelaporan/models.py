10157a4fc6eae0e7bfd91f16eb96f08169f0fd4f
ArisatoX <patricksungkharisma27@gmail.com>
2021-01-29 21:06:54 +0700

    add unit testing get_nama_perusahaan

--- e_lapor_psu/admin_pelaporan/models.py
+++ e_lapor_psu/admin_pelaporan/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from pelaporan.models import DataPerusahaan
 
 # Create your models here.
+
+def get_nama_perusahaan(id):
+
+    query = DataPerusahaan.objects.get(id_data_perusahaan_id = id)
+    return query.nama_perusahaan
