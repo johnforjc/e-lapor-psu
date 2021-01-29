eda53c0576893951bb4a3f94493e5dd296acff64
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2021-01-29 21:26:53 +0700

    add test get lokasi proyek

--- e_lapor_psu/admin_pelaporan/models.py
+++ e_lapor_psu/admin_pelaporan/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from pelaporan.models import DataPerusahaan
+from pelaporan.models import DataProyek
 
 # Create your models here.
 
@@ -7,3 +8,7 @@ def get_nama_perusahaan(id):
 
     query = DataPerusahaan.objects.get(id_data_perusahaan_id = id)
     return query.nama_perusahaan
+
+def get_lokasi_proyek(id):
+    query = DataProyek.objects.get(id_data_proyek = id)
+    return query.lokasi_proyek
--- e_lapor_psu/admin_pelaporan/tests.py
+++ e_lapor_psu/admin_pelaporan/tests.py
@@ -1,32 +1,3 @@
 from django.test import TestCase
-from pelaporan.models import DataPerusahaan
-from admin_pelaporan.models import get_nama_perusahaan
-from unittest.mock import Mock, patch
 
-class AdminTest(TestCase):
-
-    @patch('pelaporan.models.DataPerusahaan.objects')
-    def testing_get_nama_perusahaan(self, mock_get_nama_perusahaan):
-        mockObject = Mock(spec=DataPerusahaan)
-        mockObject.id_data_perusahaan = 1
-        mockObject.nama_perusahaan = 'Patrick Industries Testing'
-        mockObject.nama_pemilik = 'Patrick'
-        mockObject.bentuk_perusahaan = 'CV'
-        mockObject.alamat_perusahaan = 'Alamat Testing'
-        mockObject.tahun_berdiri = 1999
-        mockObject.no_telp = '081000000000'
-        mockObject.email = 'testing26@gmail.com'
-        mockObject.website = 'www.patrickindustries.com'
-        mockObject.verified_admin = 0
-
-        mockObject.foto_pemilik = 'Testing Folder/testing.jpg'
-        mockObject.ktp_pemilik = 'Testing Folder/testing.pdf'
-        mockObject.akta_pendirian_badan_usaha = 'Testing Folder/testing.pdf'
-
-        mock_get_nama_perusahaan.get.return_value = mockObject
-        
-        id = mockObject.id_data_perusahaan
-        expected = 'Patrick Industries Testing'
-        actual = get_nama_perusahaan(id)
-        print('HASIL NAMA PERUSAHAAN: ' + actual)
-        self.assertEqual(expected, actual)
+# Create your tests here.
